a87804ece44452a7509f65d652185d3597ec6bf3
Abitha Baby <132005925+abitha0020@users.noreply.github.com>
2023-11-07 21:51:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+let btn = document.getElementById('btn'); // Add this line to get the button element
+
+btn.addEventListener('click', handleClick);
+
+function handleClick() {
+  fetch('https://dummyjson.com/quotes/random')
+    .then(data => data.json())
+    .then(output => {
+      quote.innerHTML = output.quote;
+      author.innerHTML = output.author;
+    });
+}
